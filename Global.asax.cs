6718dec0d558879b76e7caa464b46585a7e6d89d
ebsilva <ebsilva@github.com>
2021-01-28 13:29:50 -0300

    Remove  usings desnecessários

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Net.Http;
 using System.Web.Http;
 using Ebs.App_Start;
 
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace Ebs.Models
 {
